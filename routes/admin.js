4149ee4a1ba96eac2a69efeb9ef04706fc2d197a
Victor <victorkauan.alu.lmb@gmail.com>
2021-09-14 23:07:40 -0300

    Registering categories to database from form and bug fix in the category model

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+require("../models/Category");
+const Category = mongoose.model("categories");
 
 router.get("/", (req, res) => {
   res.render("admin/index");
@@ -15,6 +18,22 @@ router.get("/categories", (req, res) => {
 
 router.get("/categories/add", (req, res) => {
   res.render("admin/addcategory");
+});
+
+router.post("/categories/new", (req, res) => {
+  const newCategory = {
+    name: req.body.name,
+    slug: req.body.slug,
+  };
+
+  new Category(newCategory)
+    .save()
+    .then(() => {
+      console.log("Category saved successfully!");
+    })
+    .catch((err) => {
+      console.log("Error saving category: " + err);
+    });
 });
 
 module.exports = router;
