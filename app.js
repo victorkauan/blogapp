b17340e0b489ade3d7609df2fcfec1f4f6b90cc1
Victor <victorkauan.alu.lmb@gmail.com>
2021-09-17 13:19:36 -0300

    Configuring Session and Connect Flash modules & creating global variables from middleware

--- app.js
+++ app.js
@@ -6,8 +6,27 @@ const mongoose = require("mongoose");
 const app = express();
 const admin = require("./routes/admin");
 const path = require("path");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 // Configurations
+// |> Session
+app.use(
+  session({
+    secret: "nodecourse",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
+// |> Middleware
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
+
 // |> Handlebars
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -29,12 +48,6 @@ mongoose
 
 // |> Public
 app.use(express.static(path.join(__dirname, "public")));
-
-// Middlewares
-app.use((req, res, next) => {
-  console.log("Hi, I'm a middleware!");
-  next();
-});
 
 // Routes
 app.get("/", (req, res) => {
