615a3017e0e922152f8568c75fc15f6b3387da9a
Victor Kauan <victorkauan@silver.bbrouter>
2021-10-06 23:03:21 -0400

    Logout system

--- routes/user.js
+++ routes/user.js
@@ -106,4 +106,10 @@ router.post("/login", (req, res, next) => {
   })(req, res, next);
 });
 
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "Successfully logged out!");
+  res.redirect("/");
+});
+
 module.exports = router;
