1ca40a43ec43b3fd1d4c190e81339bebcb96b654
Victor Kauan <victorkauan.alu.lmb@gmail.com>
2021-10-11 23:25:51 -0300

    Deploying the application on Heroku

--- config/auth.js
+++ config/auth.js
@@ -1,3 +1,4 @@
+// Loading modules
 const localStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
--- models/Category.js
+++ models/Category.js
@@ -1,3 +1,4 @@
+// Loading modules
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
